6885d877857392be7f128bb29019109841c371aa
peeCaffeine <axljohnmendoza@gmail.com>
2023-09-22 20:01:15 +0800

    basic player registration

--- index.js
+++ index.js
@@ -10,7 +10,7 @@
     Author: Coffee Delulu of C1A 2023
 */
 
-const express = require('express');
+const express = require('express')
 const app = express();
 
 const bodyParser = require('body-parser')
@@ -24,34 +24,124 @@ console.log("JPCS Code Rush Backend Service")
 
 // Database Functions
 
-db.serialize(() => {
+db.serialize(() => 
+{
     db.run(`CREATE TABLE IF NOT EXISTS players(
-        uuid TEXT PRIMARY KEY,
-        name TEXT NOT NULL,
-        top_wpm INTEGER,
-        is_admin INTEGER
+        uuid TEXT PRIMARY KEY DEFAULT '',
+        name TEXT UNIQUE DEFAULT '',
+        top_wpm INTEGER DEFAULT 0,
+        is_admin INTEGER DEFAULT 0
     )`)
 })
 
 // Routing 
 
-app.get('/', (req, res) => {
+app.get('/', (req, res) => 
+{
     console.log(req.method + ' Request From ' + req.hostname + ' > ' + req.path)
-    res.json({
+    res.json(
+    {
         message: 'CodeRush Api Works :)',
         from: 'Coffee'
     })
 })
 
-app.post('/api/v2/player', (req, res) => {
+app.post('/api/v2/player', (req, res) => 
+{
+    if (!req.body.hasOwnProperty('type'))
+    {
+        res.status(400).json(
+        { 
+            error: 'Bad Request',
+            message: "No 'type' property in api request" 
+        })
+        return
+    }
+
+    if (!req.body.hasOwnProperty('data'))
+    {
+        res.status(400).json(
+        { 
+            error: 'Bad Request',
+            message: "No 'data' property in api request" 
+        })
+        return
+    }
+
     const { type, data } = req.body;
-    console.log('Type: ' + type);
-    console.log('Data: ' + data);
+
+    console.log('API Call Type: ' + type);
+
+    if (type == 'register_player') 
+    {
+        if (!data.hasOwnProperty('uuid'))
+        {
+            res.status(400).json(
+            { 
+                error: 'Bad Request',
+                message: "No 'uuid' property in api request" 
+            })
+            return
+        }
+
+        if (!data.hasOwnProperty('name'))
+        {
+            res.status(400).json(
+            { 
+                error: 'Bad Request',
+                message: "No 'name' property in api request" 
+            })
+            return
+        }
+
+        const { uuid, name } = data;
+
+        db.run(`INSERT INTO players 
+        (
+            uuid, 
+            name
+        ) 
+        VALUES (?, ?)`, 
+        [
+            uuid, 
+            name
+        ], 
+        function (err) 
+        {
+            if (err) 
+            {
+                console.error(`Error adding new player ${name}:`, err.message);
+                res.status(400).json(
+                { 
+                    error: 'Bad Request',
+                    message: err.message
+                })
+                return
+
+            } 
+            else 
+            {
+                console.log(`New player ${name} added with row id ${this.lastID}!`)
+                res.status(200).json({ message: "OK" })
+                return
+            }
+        });
+    }
+    else
+    {
+        console.error(`Invalid Request Attempt`);
+        res.status(400).json(
+        { 
+            error: 'Bad Request',
+            message: 'Invalid'
+        })
+    }
 }) 
 
 // Listening Worker
 
-const server = app.listen(4000, function () {
+const server = app.listen(4000, function () 
+{
     let host = server.address().address
     let port = server.address().port
 })
